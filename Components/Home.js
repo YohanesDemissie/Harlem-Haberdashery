8464a122071f445e2c6bd84379f7a376f5b7746b
Yohanes Demissie <Yohanesdemissie@gmail.com>
2021-03-10 19:21:56 -0800

    added componany logo and created new component folder/files

---
+++ Components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Image } from 'react-native';
+import HarlemHaberdasheryLogo from '../Media/Logo/Harlem-Haberdashery-Logo.png';
+
+export default function Home() {
+  return(
+    <Image source={HarlemHaberdasheryLogo} />
+  );
+}
