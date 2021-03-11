4e2e9aefc781ea038ef179967db34d212e86a9a0
Yohanes Demissie <Yohanesdemissie@gmail.com>
2021-03-11 13:59:11 -0800

    added navigational functionality and tested with multi-components swapping

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 import Home from './Components/Home.js';
+import Story from './Components/Story.js';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <Home />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Story" component={Story} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ Components/Story.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text } from 'react-native';
+
+export default function Story() {
+  return(
+    <Text>
+        Story
+    </Text>
+  )
+}
